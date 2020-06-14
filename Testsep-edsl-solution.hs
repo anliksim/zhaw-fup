bf0f9f7efef7f218901e38e6140d30bcb6792340
Simon Anliker <anliksim@users.noreply.github.com>
2020-06-14 20:35:09 +0200

    Testsep edsl and questions

---
+++ Testsep-edsl-solution.hs
@@ -0,0 +1,28 @@
+-- shallow embedding
+
+newtype Polynomial = Polynomial { app :: Integer -> Integer }
+
+addP :: Polynomial -> Polynomial -> Polynomial
+addP (Polynomial p1) (Polynomial p2) = Polynomial $ \x -> p1 x + p2 x
+
+monome :: Integer -> Integer -> Polynomial
+monome coefficient exponent = Polynomial $ \x -> coefficient * x^exponent
+
+sumNotation :: [Integer] -> Polynomial
+sumNotation xs = Polynomial $
+    \x -> sum (map (\(c,e) -> c*x^e) xsIndexed)
+    where
+        xsIndexed = zip xs [0..]
+
+
+-- Welcher Term entspricht dem Polynom 'x^2+2'
+
+--    (+)   (1 * x^2)   (2 * x^1)
+a1 = addP (monome 1 2) (monome 2 0)
+
+
+-- Geben Sie zwei verschiedene Terme an, die das Polynom 'x - x^3' beschreiben
+
+--    (+)   (1 * x^2)   (2 * x^1)
+a21 = addP (monome 1 1) (monome 1 1)
+a22
